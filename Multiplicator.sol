76b7b157452cd340028cf9dfb9b11390d05b8467
Ayvaz Miftakhov <ayvaz.miftakhov@gmail.com>
2021-10-15 21:15:46 +0300

    multiplication function added

--- Multiplicator.sol
+++ Multiplicator.sol
@@ -3,10 +3,22 @@ pragma ton-solidity >= 0.35.0;
 pragma AbiHeader expire;
 
 contract Multiplicator {
+	uint public product = 1;
 
 	constructor() public {
 		require(tvm.pubkey() != 0, 101);
 		require(msg.pubkey() == tvm.pubkey(), 102);
 		tvm.accept();
+	}
+
+	modifier checkOwnerAndAccept {
+		require(msg.pubkey() == tvm.pubkey(), 102);
+		tvm.accept();
+		_;
+	}
+
+	function multiply(uint multiplier) public checkOwnerAndAccept {
+        require(multiplier >= 1 && multiplier <= 10, 201, "multiplier value must be between 1 and 10");
+		product *= multiplier;
 	}
 }
