7c3b413e19b120916241b6175f8903c0c13821c7
Ayvaz Miftakhov <ayvaz.miftakhov@gmail.com>
2021-10-17 12:35:41 +0300

    indents fixed

--- Multiplicator.sol
+++ Multiplicator.sol
@@ -3,22 +3,22 @@ pragma ton-solidity >= 0.35.0;
 pragma AbiHeader expire;
 
 contract Multiplicator {
-	uint public product = 1;
+    uint public product = 1;
 
-	constructor() public {
-		require(tvm.pubkey() != 0, 101);
-		require(msg.pubkey() == tvm.pubkey(), 102);
-		tvm.accept();
-	}
+    constructor() public {
+        require(tvm.pubkey() != 0, 101);
+        require(msg.pubkey() == tvm.pubkey(), 102);
+        tvm.accept();
+    }
 
-	modifier checkOwnerAndAccept {
-		require(msg.pubkey() == tvm.pubkey(), 102);
-		tvm.accept();
-		_;
-	}
+    modifier checkOwnerAndAccept {
+        require(msg.pubkey() == tvm.pubkey(), 102);
+        tvm.accept();
+        _;
+    }
 
-	function multiply(uint multiplier) public checkOwnerAndAccept {
+    function multiply(uint multiplier) public checkOwnerAndAccept {
         require(multiplier >= 1 && multiplier <= 10, 201, "multiplier value must be between 1 and 10");
-		product *= multiplier;
-	}
+        product *= multiplier;
+    }
 }
